e984d3aefb05ba612f875f9b509258c60035b727
Aaron Gaddes <aarongaddes@gmail.com>
2018-02-13 12:27:30 +1100

    Added a Property interface and added initial results and saved arrays

---
+++ src/app/interfaces/property.interface.ts
@@ -0,0 +1,11 @@
+export interface Property {
+    price: string,
+    agency: {
+        brandingColors: {
+            primary: string
+        },
+        logo: string
+    },
+    id: string,
+    mainImage: string
+}
