5ffb0cb9b57f72cf3bb9a735abc516e1ac9d8996
Aaron Gaddes <aarongaddes@gmail.com>
2018-02-16 14:15:41 +1100

    Updated to work without property list component + added flex container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,4 +57,14 @@ export class AppComponent implements OnInit {
       "mainImage": "http://i2.au.reastatic.net/640x480/5e84d96722dda3ea2a084d6935677f64872d1d760562d530c3cabfcb7bcda9c2/main.jpg"
     }];
   }
+
+  onSave(property) {
+    console.log(property);
+    this.saved.push(property);
+  }
+
+  onRemove(index) {
+    console.log(index);
+    this.saved.splice(index,1);
+  }
 }
--- src/app/components/property/property.component.ts
+++ src/app/components/property/property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Property } from '../../interfaces/property.interface';
 
@@ -10,11 +10,18 @@ import { Property } from '../../interfaces/property.interface';
 export class PropertyComponent implements OnInit {
 
   @Input('property') property: Property;
+  @Input('action') action: 'ADD' | 'REMOVE' = 'ADD';
+
+  @Output('onActionEmitter') onActionEmitter = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
     
+  }
+  
+  onAction() {
+    this.onActionEmitter.emit();
   }
 
 }
